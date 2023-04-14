6eb6f449c2173e49112dac45418a3b3ffa025323
김명주 <hanson@loplat.com>
2023-04-14 12:03:10 +0900

    feat: black main.py

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ import requests
 
 
 # Google Photos Library API 권한 정의
-SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+SCOPES = ["https://www.googleapis.com/auth/photoslibrary.readonly"]
 CLIENT_SECRET_FILE = "token.json"
-credFile = f'token_photo_sync_user.json'
+credFile = f"token_photo_sync_user.json"
 
 
-def download_file(url:str, destination_folder:str, file_name:str):
+def download_file(url: str, destination_folder: str, file_name: str):
     response = requests.get(url)
     if response.status_code == 200:
         file = os.path.join(destination_folder, file_name)
         if os.path.exists(file):
             print(f"Already exist file {file_name}")
             return
-        
-        print('Downloading file {0}'.format(file_name))
 
-        with open(os.path.join(destination_folder, file_name), 'wb') as f:
+        print("Downloading file {0}".format(file_name))
+
+        with open(os.path.join(destination_folder, file_name), "wb") as f:
             f.write(response.content)
+
 
 if __name__ == "__main__":
     creds = None
@@ -40,29 +41,25 @@ if __name__ == "__main__":
             flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
             creds = flow.run_local_server(port=0)
         # 성공하면 기록하도록 한다.
-        with open(credFile, 'w') as token:
+        with open(credFile, "w") as token:
             token.write(creds.to_json())
 
     # 구글 포토 API 사용
-    service = build('photoslibrary', 'v1', credentials = creds, static_discovery=False)
+    service = build("photoslibrary", "v1", credentials=creds, static_discovery=False)
 
     # 즐겨찾기한 사진 가져오기
     body = {
-            'pageSize': 100,
-            'filters': {
-                "featureFilter" :{
-                    "includedFeatures":  ['FAVORITES']    
-                },
-                "mediaTypeFilter": {
-                    "mediaTypes": ["PHOTO"]
-                }
-            }
-        }
+        "pageSize": 100,
+        "filters": {
+            "featureFilter": {"includedFeatures": ["FAVORITES"]},
+            "mediaTypeFilter": {"mediaTypes": ["PHOTO"]},
+        },
+    }
     with ThreadPoolExecutor(max_workers=50) as executor:
         response = service.mediaItems().search(body=body).execute()
         while response and (media_items := response.get("mediaItems")):
             for item in media_items:
-                download_url = item['baseUrl'] + '=d'
+                download_url = item["baseUrl"] + "=d"
                 width = item["mediaMetadata"]["width"]
                 height = item["mediaMetadata"]["height"]
 
@@ -71,7 +68,9 @@ if __name__ == "__main__":
                 else:
                     destination_folder = "/Users/hanson/Pictures/wallpaper/vertical"
                 # download_file(download_url, destination_folder, item["filename"])
-                executor.submit(download_file, download_url, destination_folder, item["filename"])
+                executor.submit(
+                    download_file, download_url, destination_folder, item["filename"]
+                )
 
             if next := response.get("nextPageToken"):
                 body.update({"pageToken": next})
